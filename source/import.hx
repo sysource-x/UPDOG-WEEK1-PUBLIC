af9fab014978035a37e9aece760a866394c8754b
sys <111463416+sysource-xyz@users.noreply.github.com>
2025-05-11 16:58:01 -0300

    lazy

--- source/import.hx
+++ source/import.hx
@@ -29,7 +29,6 @@ import funkin.Paths;
 import funkin.data.ClientPrefs;
 import funkin.data.Conductor;
 import funkin.utils.CoolUtil;
-import funkin.utils.CrashHandler;
 import funkin.data.Highscore;
 import funkin.states.*;
 import funkin.objects.BGSprite;
@@ -41,6 +40,7 @@ import mobile.flixel.FlxVirtualPad;
 import mobile.flixel.input.FlxMobileInputID;
 import mobile.backend.SUtil;
 import mobile.backend.Asset2File;
+import mobile.backend.CrashHandler;
 
 /*
 //Android
